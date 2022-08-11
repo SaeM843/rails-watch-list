c476e468f11561f37d267e551ae6062556174e67
Sae Matsuda <sae.matsuda.843@gmail.com>
2022-08-11 15:35:06 +0900

    Added List controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+# A user can see all the lists
+# GET "lists"
+# A user can see the details of a given list and its name
+# GET "lists/42"
+# A user can create a new list
+# GET "lists/new"
+# POST "lists"
+  resources :lists, only: [:index, :show, :new, :create]
 end
