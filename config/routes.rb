5e81f645b086ae4dee5cb49078a970a20651d9d2
Sae Matsuda <sae.matsuda.843@gmail.com>
2022-08-12 16:35:02 +0900

    started adding CSS

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Rails.application.routes.draw do
 # A user can create a new list
 # GET "lists/new"
 # POST "lists"
-  resources :lists, only: [:index, :show, :new, :create]  do
+  resources :lists, only: [:index, :show, :new, :create, :destroy]  do
     resources :bookmarks, only: [:new, :create,]
   end
   resources :bookmarks, only: [:destroy]
